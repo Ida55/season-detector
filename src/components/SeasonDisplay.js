1fbad05ecf5561f071a5fec0fb7f6d33efa88699
ida.pousse <idapousse@yahoo.fr>
2019-10-23 17:27:24 +0200

    Add component

--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter': 'summer';
+    }
+
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const text = season === 'winter' ? 'Bbrr, it\'s chilly out here' : 'Let\'s hit the beach';
+    const icon = season === 'winter' ? 'snowflake' : 'sun'
+
+    console.log(season);
+
     return ( 
-        <div>Season Display</div>
+        <div>
+            <i className={`icon-left massive ${icon} icon`} />
+           <h1>{text}</h1> 
+           <i className={`icon-right massive ${icon} icon`}/>
+        </div>
      );
 }
  
