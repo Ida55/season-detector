14b7adabee4ca23803c403d23da53ad9d5dfa497
ida.pousse <idapousse@yahoo.fr>
2019-10-23 15:31:42 +0200

    Add conditional rendering & method

--- src/App.js
+++ src/App.js
@@ -9,24 +9,30 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+  }
+  
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({lat: position.coords.latitude});
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({lat: position.coords.latitude}),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
   render() { 
-    return <div>
-      Latitude: {this.state.lat}
-      <br/>
-      Error: {this.state.errorMessage}
-      </div>;
-  };
-}
+    
+      if(this.state.errorMessage && !this.state.lat){
+      return <div>Error: {this.state.errorMessage}</div>
+      } 
+      
+      if(!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+      }
+
+      return <div>Loading!</div>
+
+    };
+  }
+
  
 export default App;
 
