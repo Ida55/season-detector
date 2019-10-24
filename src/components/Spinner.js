b0bdb3068d9930d01f7ab3deedb0b3784f2e593f
ida.pousse <idapousse@yahoo.fr>
2019-10-24 14:27:28 +0200

    Add component

---
+++ src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Spinner = ({message}) => {
+    return ( 
+       
+  <div className="ui active dimmer">
+    <div className="ui big text loader">{message}</div>
+  </div>
+     );
+};
+
+ 
+export default Spinner;
