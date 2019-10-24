a5badbfecda83a7173e13dbde4adbca2fe68cf78
ida.pousse <idapousse@yahoo.fr>
2019-10-24 10:34:15 +0200

    Various tweaks

--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -1,26 +1,37 @@
 import React from 'react';
 
+const seasonConfig = {
+    summer: {
+        text: 'Let\'s hit the beach',
+        iconName: 'sun'
+    },
+    winter: {
+        text:'Brrr, it\'s chilly out here!',
+        iconName: 'snowflake'
+    }
+};
+
 const getSeason = (lat, month) => {
     if(month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
     } else {
-        return lat > 0 ? 'winter': 'summer';
+        return lat > 0 ? 'winter' : 'summer';
     }
-
 }
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
-    const text = season === 'winter' ? 'Bbrr, it\'s chilly out here' : 'Let\'s hit the beach';
-    const icon = season === 'winter' ? 'snowflake' : 'sun'
+    const { text, iconName } = seasonConfig[season]; // return our object {text, iconName}
 
-    console.log(season);
+    // console.log(season);
 
     return ( 
         <div>
-            <i className={`icon-left massive ${icon} icon`} />
-           <h1>{text}</h1> 
-           <i className={`icon-right massive ${icon} icon`}/>
+            <i className={`icon-right massive ${iconName} icon`} />
+            <i className={`icon-left massive ${iconName} icon`} />
+            <h1>{text}</h1> 
+            <i className={`icon-right massive ${iconName} icon`}/>
+            <i className={`icon-left massive ${iconName} icon`} />
         </div>
      );
 }
